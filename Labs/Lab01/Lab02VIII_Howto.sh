656cc946c546450573bc87ccbb713718a611dacd
MameJenny <mema4846@colorado.edu>
2019-02-01 11:37:26 -0700

    Fixed issue with unwanted pipes and added a line to remove intermediate file

--- Labs/Lab01/Lab02VIII_Howto.sh
+++ Labs/Lab01/Lab02VIII_Howto.sh
@@ -1,1 +1,4 @@
- cut -f 2-4 -d , PredPreyData.csv > Intermediate.csv | grep time Intermediate.csv >> Final.csv | tail Intermediate.csv >> Final.csv
+ cut -f 2-4 -d , PredPreyData.csv > Intermediate.csv 
+grep time Intermediate.csv > Final.csv 
+tail Intermediate.csv >> Final.csv
+rm Intermediate.csv
